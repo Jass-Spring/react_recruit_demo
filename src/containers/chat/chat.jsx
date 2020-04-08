c01f42c078f44c55ed7f5ed9ab043ed67819e01c
Jass-Spring <1250793617@qq.com>
2020-04-08 17:29:06 +0800

    显示聊天内容

--- src/containers/chat/chat.jsx
+++ src/containers/chat/chat.jsx
@@ -22,21 +22,50 @@ class Chat extends Component {
   }
 
   render () {
+    const { user } = this.props
+    const { users, chatMsgs } = this.props.chat
+
+    const myId = user._id
+    if (!users[myId]) {
+      return null
+    }
+    const targetId = this.props.match.params.userid
+    const chatId = [ myId, targetId ].sort().join('_')
+
+    const msgs = chatMsgs.filter(msg => msg.chat_id === chatId)
+
+    const targetName = users[targetId].username
+    const targetHeader = users[targetId].header
+    const targetIcon = targetHeader ? require(`../../assets/images/${targetHeader}.png`) : null
+
     return (
       <div id="chat-page">
-        <NavBar>aa</NavBar>
+        <NavBar>{targetName}</NavBar>
         <List>
-          <Item
-            thumb={require(`../../assets/images/头像1.png`)}
-          >
-            你好
-          </Item>
-          <Item
-            className="chat-me"
-            extra="我"
-          >
-            你好
-          </Item>
+          {
+            msgs.map(msg => {
+              if (myId === msg.to) {
+                return (
+                  <Item
+                    key={msg._id}
+                    thumb={targetIcon}
+                  >
+                    {msg.content}
+                  </Item>
+                )
+              } else {
+                return (
+                  <Item
+                    key={msg._id}
+                    className="chat-me"
+                    extra="我"
+                  >
+                    {msg.content}
+                  </Item>
+                )
+              }
+            })
+          }
         </List>
 
         <div className="am-tab-bar">
@@ -55,6 +84,6 @@ class Chat extends Component {
 }
 
 export default connect(
-  state => ({ user: state.user }),
+  state => ({ user: state.user, chat: state.chat }),
   { sendMsg }
 )(Chat)
